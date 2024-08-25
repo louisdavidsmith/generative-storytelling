5a2791f3a65ac81199c21ecd4bfd0ea809d62bd0
Louis Smith <louis@pop-os.localdomain>
2024-08-25 12:20:09 -0400

    add file

--- narrative/generation/cmd/main.go
+++ narrative/generation/cmd/main.go
@@ -2,43 +2,55 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
 	"net"
+	"os"
 
 	"google.golang.org/grpc"
 	pb "generation/pkg/proto/generation"
-
+	"generation/pkg/dummy"
 	"github.com/gage-technologies/mistral-go"
+	"generation/pkg/mistral"
 )
 
 type GenerationServiceServer struct {
-	generation.UnimplementedGenerationServiceServer
-	Client *mistral.NewMistralClientDefault
-	ModelId string
+	pb.UnimplementedGenerationServiceServer
+	Generator generation.Generator
 }
 
 func (s *GenerationServiceServer) GenerateResponseStream(req *pb.GenerateResponseRequest, stream pb.GenerationService_GenerateResponseStreamServer) error {
-	log.Printf("Received request: %v", req)
-	for i := 0; i < 5; i++ {
-		if err := stream.Send(&pb.StreamResponse{Token: "dummy token"}); err != nil {
-			return err
-		}
-	}
-	return nil
+	return s.Generator.GenerateResponseStream(req, stream)
 }
 
-func (s *GenerationServiceServer) GenerateResponseBatch(context.Context, *pb.GenerateResponseRequest) (*pb.BatchResponse, error) {
-	return &pb.BatchResponse{Response: "dummy response"}, nil
+func (s *GenerationServiceServer) GenerateResponseBatch(ctx context.Context, req *pb.GenerateResponseRequest) (*pb.BatchResponse, error) {
+	return s.Generator.GenerateResponseBatch(ctx, req)
 }
 
 func main() {
+	generatorType := flag.String("generator", "dummy", "The type of generator to use (dummy, mistral)")
+	flag.Parse()
+
+	var generator generation.Generator
+
+	switch *generatorType {
+	case "dummy":
+		generator = dummy.NewDummyGenerator()
+	case "mistral":
+		mistralClient := mistral.NewMistralClientDefault()
+		modelId := os.Getenv("MISTRAL_MODEL_ID")
+		generator = mistral.NewMistralGenerator(mistralClient, modelId)
+	default:
+		log.Fatalf("Unsupported generator type: %s", *generatorType)
+	}
+
 	lis, err := net.Listen("tcp", ":50052")
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 
 	grpcServer := grpc.NewServer()
-	generation.RegisterGenerationServiceServer(grpcServer, &GenerationServiceServer{})
+	pb.RegisterGenerationServiceServer(grpcServer, &GenerationServiceServer{Generator: generator})
 	log.Printf("Server is running on port 50052")
 
 	if err := grpcServer.Serve(lis); err != nil {
---
+++ narrative/generation/go.mod
@@ -0,0 +1,16 @@
+module generation
+
+go 1.23.0
+
+require (
+	github.com/gage-technologies/mistral-go v1.1.0
+	google.golang.org/grpc v1.65.0
+	google.golang.org/protobuf v1.34.2
+)
+
+require (
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240528184218-531527333157 // indirect
+)
